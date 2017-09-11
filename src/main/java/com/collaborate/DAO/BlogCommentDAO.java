f4a18b658ecc90a3abe71cd03d07ae62350828d6
srinathboddu22 <srinathboddu22@gmail.com>
2017-09-11 13:48:41 +0530

    BlogComment updated

--- src/main/java/com/collaborate/DAO/BlogCommentDAO.java
+++ src/main/java/com/collaborate/DAO/BlogCommentDAO.java
@@ -1,5 +1,15 @@
 package com.collaborate.DAO;
 
+import java.util.List;
+
+
+import com.collaborate.Model.BlogComment;
+
 public interface BlogCommentDAO {
 
+	public boolean createBlog(BlogComment blogcomment);
+	public BlogComment getBlogcommentdetails(int id);
+	public List<BlogComment> getBlogComment();
+    public boolean editBlogComment(int id);
+	public boolean deleteBlogComment(int id);
 }
