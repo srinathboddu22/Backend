4b56909f1b69b48124ad659f7b5d096c4330bd02
srinathboddu22 <srinathboddu22@gmail.com>
2017-09-13 20:51:29 +0530

    forum testing

---
+++ src/test/java/com/collaborate/Backend/ForumDAOTestCase.java
@@ -0,0 +1,80 @@
+package com.collaborate.Backend;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.collaborate.DAO.ForumDAO;
+import com.collaborate.Model.Forum;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+public class ForumDAOTestCase {
+	static ForumDAO forumDAO;
+	static Forum forum;
+	@BeforeClass
+	public static void initialize()
+	{
+		AnnotationConfigApplicationContext annotationConfigAppContext=new AnnotationConfigApplicationContext();
+		annotationConfigAppContext.scan("com.collaborate");
+		annotationConfigAppContext.refresh();
+		forumDAO=(ForumDAO)annotationConfigAppContext.getBean("forumDAO");
+	    forum=(Forum)annotationConfigAppContext.getBean("forum");
+	}
+	
+	@Test
+	public void createForumtest()
+	{
+		//Forum forum=new Forum();
+		forum.setForumId(111);
+		forum.setForumName("Dravid");
+		forum.setForumContent("Dravid is a Cricket Coach");
+		
+		forum.setCreateDate(new java.util.Date());
+		forum.setStatus("NA");
+		
+		assertTrue("Problem in Forum Creation",forumDAO.createForum(forum));
+	
+	}
+	
+	
+	@Test
+	public void editForumTest()
+	{
+	Forum forum = new Forum();
+	forum.setForumId(111);
+	forum.setForumName("Dravid");
+	forum.setForumContent("Dravid plays Cricket ");
+	forum.setCreateDate(new java.util.Date());
+	forum.setUserId(1001);
+	forum.setStatus("NA");
+	assertTrue("Problem in approving forum",forumDAO.editForum(forum.getForumId()));	
+	}
+	
+	
+	@Test
+	public void deleteForumTest()
+	{
+	Forum forum = new Forum();
+	forum.setForumId(111);
+	assertTrue("Problem in approving Forum",forumDAO.deleteForum(forum.getForumId()));	
+	}
+	
+	@Ignore
+	@Test
+	public void getForumTest()
+	{
+	Forum forum = new Forum();
+	forum.setForumId(111);
+	}
+
+	@Test
+	public void approveForumTest()
+	{
+		Forum forum=new Forum();
+		forum.setForumId(111);
+		forum.setForumName("Dravid");
+		forum.setForumContent("Dravid is a Cricket Coach");
+		forum.setCreateDate(new java.util.Date());
+		forum.setStatus("NA");
+		assertTrue("Problem in approving Forum",forumDAO.approveForum(forum));	
+	}
+}
