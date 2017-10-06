5c1f5416d72957fcc169bdadcb8a9c4e2c9ee1eb
srinathboddu22 <srinathboddu22@gmail.com>
2017-10-06 18:33:50 +0530

    User dao updated

--- src/main/java/com/collaborate/DAO/UserDAO.java
+++ src/main/java/com/collaborate/DAO/UserDAO.java
@@ -7,13 +7,15 @@ import com.collaborate.Model.User;
 
 public interface UserDAO {
 
-	public boolean createUser(User user);
-	public User getUser(int userId);
-	public List<User> getUsers();
+	
 	public boolean approveUser(User user);
 	public boolean editUser(int userId);
 	public boolean deleteUser(int userId);
 	public boolean isUsernameValid(String username);
 	public boolean registerUser(User user);
 	public boolean isEmailValid(String email);
+	User login(User user);
+	void update(User user);
+	User getUserByUsername(String username);
+	public boolean isUpdatedEmailValid(String email,String username);
 }
